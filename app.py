b12cb5328c9988c9fd681cbd449a481fdb60f639
Jennylidya <25bcnb20@kristujayanti.com>
2026-03-25 18:29:42 +0530

    fix port

--- app.py
+++ app.py
@@ -35,5 +35,8 @@ def submit():
     return "Data Saved"
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
