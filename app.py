d662fcdcb57be3e8a8605d3758bcf7b4a6817d88
Jennylidya <25bcnb20@kristujayanti.com>
2026-03-25 17:34:27 +0530

    portfolio project

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+def init_db():
+    conn = sqlite3.connect("database.db")
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS contacts (name TEXT, email TEXT, message TEXT)"
+    )
+    conn.close()
+
+init_db()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form["name"]
+    email = request.form["email"]
+    message = request.form["message"]
+
+    conn = sqlite3.connect("database.db")
+    conn.execute(
+        "INSERT INTO contacts VALUES (?, ?, ?)",
+        (name, email, message),
+    )
+    conn.commit()
+    conn.close()
+
+    return "Data Saved"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
